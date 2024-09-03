ffdf398f80b171226c7f8f86704812fe61509a2d
Tim Witkovsky <tim.witkov@gmail.com>
2024-09-02 20:38:02 -0400

    finished with Ch 2, getting ready for Ch 3, ASSteroids

--- main.py
+++ main.py
@@ -2,19 +2,39 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updateable:
+            obj.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit fps to 60fps
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
