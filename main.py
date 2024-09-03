c36dda93df4153a6eb689488dde56aa5d54f6676
Tim Witkovsky <tim.witkov@gmail.com>
2024-09-02 22:49:10 -0400

    finished the project

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 from constants import *
 
 
@@ -14,7 +18,14 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updateable, drawable)
     Player.containers = (updateable, drawable)
+    AsteroidField.containers = updateable
+    Shot.containers = (shots, updateable, drawable)
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -25,6 +36,16 @@ def main():
 
         for obj in updateable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
